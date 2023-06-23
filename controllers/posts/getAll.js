1e3d9c8ec6ed314c3aefbb6f9515cdb0f1944114
Yaroslaw Kyrylenko <kirik.slaw@gmail.com>
2023-06-23 10:48:53 +0300

    updated get all posts controller, added two options - all and subscribes

--- controllers/posts/getAll.js
+++ controllers/posts/getAll.js
@@ -1,15 +1,26 @@
-const { Post } = require('../../models')
+const { Post, User } = require('../../models')
 const paginationQuery = require('../../helpers/paginationQuery')
 const PostsAggregation = require('../../helpers/postsAggregation')
+const { Types } = require('mongoose')
 
 const getAll = async (req, res) => {
   const { currentUserId, currentUser } = req
   const { arrayOfId } = paginationQuery(req.query)
-
   const postAggregations = new PostsAggregation(currentUserId)
 
+  const match = {
+    _id: { $nin: arrayOfId }
+  }
+
+  if (req.query.type === 'subscribes') {
+    match.$and = [
+      { author: { $ne: new Types.ObjectId(currentUserId) } },
+      { author: { $in: currentUser.subscribes.map((el) => new Types.ObjectId(el)) } }
+    ]
+  }
+
   const pipeline = [
-    { $match: { _id: { $nin: arrayOfId } } },
+    { $match: match },
     ...postAggregations.standardPipeline(currentUser.collections, req.query)
   ]
 
